bdab1b5d987e2107190f47bef79d9bf6394c3860
Hgiddal <helena.barddal@escola.pr.gov.br>
2025-07-15 14:17:25 -0300

    retirando os cartoes

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+   'Vida marinha',
+    'Qual é a maior espécie de tubarão?',
+    'A maior espécie de tubarão é o Tubarão Baleia, ele pode atingir até 20 metros de comprimento'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a diferença entre um número racional e um número irracional?',
+    'A principal diferença entre números racionais e irracionais está na sua capacidade de serem representados como frações' 
+)
+
+criaCartao(
+    'Perguntas gerais',
+    'Quem foi a primeira pessoa a viajar para o espaço?',
+    'A primeira pessoa a viajar para o espaço foi o cosmonauta soviético Yuri Gagarin, em 12 de abril de 1961'
+)
