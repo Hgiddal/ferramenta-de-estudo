b0c706b67a8b25039aadde9ce29942a0f799f927
Hgiddal <helena.barddal@escola.pr.gov.br>
2025-07-30 08:34:32 -0300

    Atualização

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -15,3 +15,15 @@ criaCartao(
     'Quem foi a primeira pessoa a viajar para o espaço?',
     'A primeira pessoa a viajar para o espaço foi o cosmonauta soviético Yuri Gagarin, em 12 de abril de 1961'
 )
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo?',
+    'O menor país do mundo é o Vaticano, com uma área de apenas 0,44 km²'
+)
+
+criaCartao(
+    'Física',
+    'Quais são os principais ramos da física?',
+    'Os principais ramos da física são: Mecânica, Termologia, Óptica, Ondulatória, Eletromagnetismo e Física Moderna.'
+)
