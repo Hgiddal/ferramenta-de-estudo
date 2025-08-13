67170171b26e06599e1d466e97562f391aac6844
Hgiddal <helena.barddal@escola.pr.gov.br>
2025-08-13 07:38:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,18 @@
   cartao.className = 'cartao' 
 
   cartao.innerHTML = `
-  
-}
+  <div class="cartao__conteudo">
+                    <h3>Vida marinha</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>Quanto tempo em média vive uma tartaruga?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                       <p>A expectativa de vida de uma tartaruga varia muito dependendo da espécie, mas, em geral, elas
+                        podem viver entre 30 e mais de 100 anos.</p>
+                    </div>
+                </div>
+                `
+  }
+
 
 
