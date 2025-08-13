721ee1d4ae744cdc78f48e395fa6b9bee8f072e7
Hgiddal <helena.barddal@escola.pr.gov.br>
2025-08-13 08:26:20 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+   'Vida marinha',
+    'Qual é a maior espécie de tubarão?',
+    'A maior espécie de tubarão é o Tubarão Baleia, ele pode atingir até 20 metros de comprimento'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a diferença entre um número racional e um número irracional?',
+    'A principal diferença entre números racionais e irracionais está na sua capacidade de serem representados como frações' 
+)
+
+criaCartao(
+    'Perguntas gerais',
+    'Quem foi a primeira pessoa a viajar para o espaço?',
+    'A primeira pessoa a viajar para o espaço foi o cosmonauta soviético Yuri Gagarin, em 12 de abril de 1961'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo?',
+    'O menor país do mundo é o Vaticano, com uma área de apenas 0,44 km²'
+)
+
+criaCartao(
+    'Física',
+    'Quais são os principais ramos da física?',
+    'Os principais ramos da física são: Mecânica, Termologia, Óptica, Ondulatória, Eletromagnetismo e Física Moderna.'
+)
