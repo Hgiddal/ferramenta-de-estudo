771608b903653e8b5cd8a782e7249be7808cc3a1
Hgiddal <helena.barddal@escola.pr.gov.br>
2025-08-13 07:46:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,17 +5,18 @@
 
   cartao.innerHTML = `
   <div class="cartao__conteudo">
-                    <h3>Vida marinha</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
-                        <p>Quanto tempo em média vive uma tartaruga?</p>
+                        <p>${pergunta}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
-                       <p>A expectativa de vida de uma tartaruga varia muito dependendo da espécie, mas, em geral, elas
-                        podem viver entre 30 e mais de 100 anos.</p>
+                       <p>${resposta}</p>
                     </div>
                 </div>
                 `
+  container.appendChild(cartao) 
   }
+
 
 
 
